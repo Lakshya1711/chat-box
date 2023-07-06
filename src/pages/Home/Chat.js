85bcfc7d27e5687968b157f4ab483087edf4a15f
Lakshya1711 <Lucky17sh@gmail.com>
2023-07-06 22:41:25 +0530

    Initial chat

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -5,6 +5,7 @@ import ChatBottom from '../../components/chat-window/bottom';
 import { useParams } from 'react-router';
 import { useRooms } from '../../context/rooms.context';
 import { Loader } from 'rsuite';
+import { CurrentRoomProvider } from '../../context/current-room.context';
 
 const Chat = () => {
   const { chatId } = useParams();
@@ -19,8 +20,15 @@ const Chat = () => {
   if (!currentRoom) {
     return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
   }
+
+  const { name, description } = currentRoom;
+
+  const currentRoomData = {
+    name,
+    description,
+  };
   return (
-    <>
+    <CurrentRoomProvider data={currentRoomData}>
       <div className="chat-top">
         <ChatTop />
       </div>
@@ -30,7 +38,7 @@ const Chat = () => {
       <div className="chat-bottom">
         <ChatBottom />
       </div>
-    </>
+    </CurrentRoomProvider>
   );
 };
 
