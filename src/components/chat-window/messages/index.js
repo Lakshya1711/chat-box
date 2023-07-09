f221bbcbf5008de0213d3be69b8ed020c9125ea0
Lakshya1711 <Lucky17sh@gmail.com>
2023-07-09 19:02:49 +0530

    Added Admin permission grant

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,8 +1,9 @@
 import { database } from '../../../misc/firebase';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
+import { Alert } from 'rsuite';
 
 const Messages = () => {
   const { chatId } = useParams();
@@ -25,11 +26,35 @@ const Messages = () => {
     };
   }, [chatId]);
 
+  const handleAdmin = useCallback(
+    async uid => {
+      const adminRef = database.ref(`/rooms/${chatId}/admins`);
+
+      let alertMsg;
+      await adminRef.transaction(admins => {
+        if (admins) {
+          if (admins[uid]) {
+            admins[uid] = null;
+            alertMsg = 'Admin Permission Removed';
+          } else {
+            admins[uid] = true;
+            alertMsg = 'Admin Permission Granted';
+          }
+        }
+        return admins;
+      });
+      Alert.info(alertMsg, 4000);
+    },
+    [chatId]
+  );
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No Messages Yet !!</li>}
       {canShowMessages &&
-        messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+        messages.map(msg => (
+          <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} />
+        ))}
     </ul>
   );
 };
